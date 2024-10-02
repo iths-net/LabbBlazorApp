15a18afc20d7aaa9797021e664dc5e3f9f083157
crunchyfrog <javaveryhot@protonmail.ch>
2024-10-02 22:14:27 +0200

    remove validation attributes from address properties

--- LabbBlazorApp/Address.cs
+++ LabbBlazorApp/Address.cs
@@ -1,17 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace LabbBlazorApp
+﻿namespace LabbBlazorApp
 {
 	public class Address
 	{
-		[Required]
-		[StringLength(40, MinimumLength = 5)]
 		public string Street { get; set; }
-		[Required]
-		[StringLength(30, MinimumLength = 2)]
 		public string City { get; set; }
-		[Required]
-		[StringLength(10, MinimumLength = 2)]
 		public string ZipCode { get; set; }
 	}
 }
